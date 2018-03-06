38a490cd396cc71604628bb79f8e5572a8713e0d
RehamEzzat <rehamezzatmohamed@gmail.com>
2018-03-06 18:05:24 +0200

    solve errors

--- src/main/java/com/swe/project/Swe2projectApplication.java
+++ src/main/java/com/swe/project/Swe2projectApplication.java
@@ -3,6 +3,7 @@ package com.swe.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 //@RestController
